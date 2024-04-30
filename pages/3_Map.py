7b2b65c89232825c75152795639ff80e0eddf218
AgoesSab <110345561+AgoesSab@users.noreply.github.com>
2024-04-30 17:03:14 +0700

    Update 3_Map.py

--- pages/3_Map.py
+++ pages/3_Map.py
@@ -4,8 +4,8 @@ import streamlit as st
 from streamlit_folium import st_folium
 
 # center on Liberty Bell, add marker
-m = folium.Map(location=[106.8410, -6.1574], zoom_start=16)
-folium.Marker([106.8410, -6.1574], popup="Liberty Bell", tooltip="Liberty Bell").add_to(m)
+m = folium.Map(location=[ -6.1574, 106.8410], zoom_start=16)
+folium.Marker([-6.1574, 106.8410], popup="Liberty Bell", tooltip="Liberty Bell").add_to(m)
 
 # call to render Folium map in Streamlit
 st_data = st_folium(m, width=725)
